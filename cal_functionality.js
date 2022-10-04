6bc85ee059627d379a3ccd5498500c487ae79d1f
Mathias Ranna <mathiasranna@gmail.com>
2022-10-04 15:42:11 +0300

    Fixed 0 button.

--- cal_functionality.js
+++ cal_functionality.js
@@ -29,23 +29,47 @@ class Calculator {
 
     chooseOperator(sign) {
         this.currOperation = sign;
-        this.prevVal = this.currVal;
+        if (this.currVal !== ""){
+            this.prevVal = this.currVal;
+        }
         this.currVal = "";
     }
 
     compute() {
-        if (this.currOperation === "÷") {
-            this.currVal = parseInt(this.prevVal) / parseInt(this.currVal);
-        } else if (this.currOperation === "×") {
-            this.currVal = parseInt(this.prevVal) * parseInt(this.currVal);
-        } else if (this.currOperation === "-") {
-            this.currVal = parseInt(this.prevVal) - parseInt(this.currVal);
-        } else if (this.currOperation === "+") {
-            this.currVal = parseInt(this.prevVal) + parseInt(this.currVal);
-        } else {
-            this.currVal = "Error";
+        if (this.currVal !== ""){
+            if (this.currOperation === "÷") {
+                this.currVal = this.round(parseFloat(this.prevVal) / parseFloat(this.currVal));
+            } else if (this.currOperation === "×") {
+                this.currVal = this.round(parseFloat(this.prevVal) * parseFloat(this.currVal));
+            } else if (this.currOperation === "-") {
+                this.currVal = this.round(parseFloat(this.prevVal) - parseFloat(this.currVal));
+            } else if (this.currOperation === "+") {
+                this.currVal = this.round(parseFloat(this.prevVal) + parseFloat(this.currVal));
+            } else {
+                this.currVal = "Error";
+            }
+            this.updateDisplay()
         }
-        this.updateDisplay()
+    }
+
+    round(value) {
+        const valueStr = value.toString();
+        if (valueStr.length > 5) {
+            const firstThreeDig = valueStr.substring(0,3);
+            const lastDig = (Math.round(parseFloat(valueStr.substring(3,4) + "." + valueStr.substring(4,)))).toString();
+            if (value > 1){
+                const firstThreeDig = valueStr.substring(0,3);
+                const lastDig = (Math.round(parseFloat(valueStr.substring(3,4) + "." + valueStr.substring(4,)))).toString();
+                const stepOfTen = valueStr.substring(4,).length
+                return firstThreeDig + lastDig + "e" + stepOfTen;
+            } else {
+                const firstFourDig = valueStr.substring(0,4);
+                const lastDig = (Math.round(parseFloat(valueStr.substring(4,5) + "." + valueStr.substring(5,)))).toString();
+                return firstFourDig + lastDig;
+            }
+
+        }
+        return value.toString();
     }
 
     updateDisplay() {
@@ -65,37 +89,59 @@ numberButtons.forEach(button => {
         calculator.appendNumber(button.innerHTML);
         calculator.updateDisplay();
         // Change operator color back to normal if not null
-        if (calculator.currOperation != null) {
-            operation.forEach(operator => {
-                operator.parentNode.style.background = "#f69906";
-                operator.style.color = "#ffffff";
-            })
-        }
+        reset_operation_style();
     })
+})
+
+document.getElementById('zero-btn').addEventListener("click", () => {
+    if (calculator.currVal != null){
+        document.getElementById('C-btn').innerHTML = "C";
+        highlight(document.getElementById('zero-btn'), "#474747");
+        calculator.appendNumber(document.getElementById('zero-btn').children[0].innerHTML);
+        calculator.updateDisplay();
+    }
 })
 
 /* Operations */
 operation.forEach(operator => {
     operator.addEventListener("click", () => {
+        if (calculator.currOperation != null){
+            calculator.compute();
+        }
         calculator.chooseOperator(operator.innerHTML);
-        operator.parentNode.style.background = "#ffffff";
-        operator.style.color = "#f69906";
+        highlight_operation(operator);
     })
 })
 
 /* Equals button */
 equalsButton.addEventListener("click", () => {
     calculator.compute();
+    highlight_operation()
 })
 
 /* Clear Button */
 document.getElementById('C-btn').addEventListener("click", () => {
+    highlight(document.getElementById('C-btn').parentNode, "#d6d6d6");
     if (document.getElementById('C-btn').innerHTML === "AC") {
         calculator.clear();
+        reset_operation_style();
     } else {
         calculator.delete();
         document.getElementById('C-btn').innerHTML = "AC";
     }
+})
+
+/* Change sign button */
+document.getElementById('change-sign').addEventListener("click", () => {
+    highlight(document.getElementById('change-sign').parentNode, "#d6d6d6");
+    if (calculator.currVal != null){
+        if (calculator.currVal[0] !== "-"){
+            calculator.currVal = "-" + calculator.currVal;
+        } else {
+            calculator.currVal = calculator.currVal.substring(1);
+        }
+    }
+    calculator.updateDisplay();
 })
 
 /* Highlight when click */
@@ -105,7 +151,23 @@ function highlight(obj, color_code) {
     setTimeout(function () {
         obj.style.background = orig;
     }, 100);
-
-
 }
 
+/* Highlight operation */
+function highlight_operation(operator) {
+    if (calculator.currOperation === null) {
+        operator.parentNode.style.background = "#ffffff";
+        operator.style.color = "#f69906";
+    } else {
+        reset_operation_style();
+        operator.parentNode.style.background = "#ffffff";
+        operator.style.color = "#f69906";
+    }
+}
+
+function reset_operation_style(){
+    operation.forEach(operator => {
+        operator.parentNode.style.background = "#f69906";
+        operator.style.color = "#ffffff";
+    })
+}
